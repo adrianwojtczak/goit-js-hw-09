7f258c6caffbdd9b2b8aaf8f460f67e02c5aa385
Adrian Wojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2023-04-13 22:03:15 +0200

    update task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let timerId = null;
 
+// Color changer
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// Set buttons
+btnStop.setAttribute('disabled', true);
+
+btnStart.addEventListener('click', () => {
+  btnStart.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+  document.body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  btnStop.setAttribute('disabled', true);
+  btnStart.removeAttribute('disabled');
+  clearInterval(timerId);
+});
